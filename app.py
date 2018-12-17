2a091a244d2437d548c636fe8138d3ceedfc13b6
Rafael Marques <rafael.marques@confitec.com.br>
2018-12-17 17:42:56 -0200

    added marshmallow dependency

--- app.py
+++ app.py
@@ -2,4 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/auth/users', methods=['POST'])
+def register():
+    pass
+
+
+@app.route('/auth/login', methods=['POST'])
+def register():
+    pass
+
+
 app.run()
